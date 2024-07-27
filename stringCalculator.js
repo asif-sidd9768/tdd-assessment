9a57c19f8bdffeac52da1eb95e5531eda0650729
asif-sidd9768 <siddiqueasif421@gmail.com>
2024-07-27 10:39:52 +0530

    Implementation: handling two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,8 @@ const add = (str) => {
   if(str === "") return 0
   if(!str.includes(",")) return parseInt(str)
   
+  const numbers = str.split(",").map(num => parseInt(num))
+  return numbers[0] + numbers[1]
 }
 
 module.exports = add
