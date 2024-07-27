f1fc0f629b29589ced8431fc914cda6c0f497bcc
asif-sidd9768 <siddiqueasif421@gmail.com>
2024-07-27 11:00:04 +0530

    Implementation: Handle custom delimeter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,13 @@
 const add = (str) => {
   if(str === "") return 0
-  if(!str.includes(",")) return parseInt(str)
+  if(str.length === 1) return parseInt(str)
   
+  if(str.startsWith('//')){
+    const parts = str.split('\n')
+    const delimeter = parts[0][2]
+    str = parts[1].split(delimeter).join(',')
+  }
+
   const numbers = str.replace(/\n/g, ',').split(",").map(num => parseInt(num))
   return numbers.reduce((sum, currNum) => sum + parseInt(currNum), 0)
 }
