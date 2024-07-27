475adee5eed8e4a04e69bfa0df67cc28a9633b5d
asif-sidd9768 <siddiqueasif421@gmail.com>
2024-07-27 10:34:49 +0530

    Test: Add test for single number return itself.

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -5,5 +5,10 @@ describe('String Calculator', () => {
   test('should return 0 for empty string', () => {
     expect(add('')).toBe(0)
   })
+
+  test('should return number itself for single number', () => {
+    expect(add('1')).toBe(1)
+  })
+  
   
 })
