f9145ffb3077a7383d39824a95e3250a6b67275c
asif-sidd9768 <siddiqueasif421@gmail.com>
2024-07-27 11:42:33 +0530

    Test: Add test to handle multiple delimeters

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -38,5 +38,8 @@ describe('String Calculator', () => {
     expect(add('//[***]\n1***2***3')).toBe(6)
   })
   
+  test('should handle multiple delimeters', () => {
+    expect(add('//[*][%]\n1*2%3')).toBe(6)
+  })
   
 })
